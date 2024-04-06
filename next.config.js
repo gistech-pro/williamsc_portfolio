73153acb79e843816a6b9b6f50997dd56ff8f25a
C. Williams <chinwawilliams@gmail.com>
2024-04-06 04:33:33 +0100

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isProd = process.env.NODE_ENV === 'production';
-const assetPrefix =  isProd ? '':'';
+const assetPrefix =  isProd ? '/williamsc_portfolio/' : '',
+  //'':'';
   //'/williamsc_portfolio/' : '',
 
 const nextConfig = {
